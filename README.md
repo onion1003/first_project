62836bd37bbbe16514c265a9aac9b31b46c36549
onion1003 <xucong@xucongdeMacBook-Air.local>
2016-05-25 11:01:22 +0800

    根据第六章内容作出修改

--- README.md
+++ README.md
@@ -12,3 +12,23 @@ element.childNodes 这是包含element元素的所有子元素的数组
 * nodeType属性，元素节点为1，属性节点为2，文本节点为3.
 * nodeValue可以得到节点的属性值，对于<p>节点来说，其第一个子节点的属性值才是<p>元素中包含的文本值
 * node.firstChild 等价于 node.childNodes[0]  node.lastChild 等价于 node.childNodes[node.childNodes.length - 1]
+
+
+##第六章笔记
+###图片库改进版
+
+1. 已经可以平稳退化了
+2. 分离js，并进行错误检查
+* 检查getElementById,getElementsByTagName这些是否可用
+* 检查需要的那些id，title是否存在
+* 使用nodeName属性来测试placeholder元素是否存在，nodeName属性会返回大写字母的值
+* 使用nodeValue属性来测试description元素的第一个子元素是否为文本节点
+3. 增加键盘访问
+* 最好不要加，弊大于利
+4. 共享onload
+* 在多个函数需要onload的时候，将其添加到队列中
+
+###DOM Core 和 HTML-DOM
+    document.getElementsByTagName("form") ==> document.forms
+    element.getAttribute("src") ==> element.src
+    placeholder.setAttribute("src", source) ==> placeholder.src = source
--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,3 +1,4 @@
+/*
 function showPic(whichPic){
     var source = whichPic.getAttribute("href");
     var alter = whichPic.getAttribute("alt");
@@ -16,3 +17,53 @@ function countBodyChildren(){
 }
 
 window.onload = countBodyChildren;
+*/
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for( var i=0; i < links.length; i++) {
+        links[i].onclick = function(){
+            return showPic(this) ? false : true; //在点击无效时，继续执行href中的链接；点击有效时不跳转
+        }
+    }
+}
+
+function showPic(whichPic) {
+    if(!document.getElementById("placeholder")) return false;
+    var source = whichPic.getAttribute("href");
+    var alter = whichPic.getAttribute("alt");
+    var placeholder = document.getElementById("placeholder");
+    if (placeholder.nodeName != "IMG") return false;
+    placeholder.setAttribute("src", source);
+    placeholder.setAttribute("alt",alter);
+    if (document.getElementById("description")) {
+        var text = whichPic.getAttribute("title") ? whichPic.getAttribute("title") : "";
+        var description = document.getElementById("description");
+        if (description.firstChild.nodeType == 3) {
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
+}
+
+/* 共享onload，可以一次onload多个函数
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery);
+*/
+
+window.onload = prepareGallery;
